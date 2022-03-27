43004b147563c6615a69688be3256ecf9db5324d
Ron Green <8326+rogreen@users.noreply.gitlab.cee.redhat.com>
2022-03-27 13:32:00 +0300

    fix(readme): add notes
    - remove namespace from postgres-DB
    - add more checks to deploy/config/is_*
    - remove deps to Taskfile to make startup/teardown easier

--- deploy/config/is_down.sh
+++ deploy/config/is_down.sh
@@ -4,6 +4,7 @@ set -eEuo pipefail
 
 CURRENT_DIR="$(dirname "$0")"
 
+./bin/kustomize build "${CURRENT_DIR}" | oc get -f - --ignore-not-found
 
 if [[ $(./bin/kustomize build "${CURRENT_DIR}" | ./bin/kubectl-slice -f - --include-kind=Deployment --stdout | oc get -f - --ignore-not-found | wc -l ) -eq 0 ]]; then
 	echo "there are no Deployemnts, no need to wait for them to be deleted"
--- deploy/config/is_up.sh
+++ deploy/config/is_up.sh
@@ -4,4 +4,6 @@ set -eEuo pipefail
 
 CURRENT_DIR="$(dirname "$0")"
 
+./bin/kustomize build "${CURRENT_DIR}" | oc get -f -
+./bin/kustomize build "${CURRENT_DIR}" | ./bin/kubectl-slice -f - --include-kind=Subscription --stdout | oc wait -f - --for=condition=CatalogSourcesUnhealthy=false
 ./bin/kustomize build "${CURRENT_DIR}" | ./bin/kubectl-slice -f - --include-kind=PostgresCluster --stdout | oc wait  -f - --for=condition=PGBackRestRepoHostReady
