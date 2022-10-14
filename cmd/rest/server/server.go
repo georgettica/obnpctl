4b25c4889dbbe71946057edf97bf782ac7066346
Ron Green <11993626+georgettica@users.noreply.github.com>
2022-10-14 14:18:43 +0300

    chore(Taskfile): version update (#71)
    * chore(Taskfile): version update
    
    - remove testing on go1.17
    - add more disabled linters when someone wants to add them
    - make the test use the go.mod file instread of the default (which is go1.17)

--- cmd/rest/server/server.go
+++ cmd/rest/server/server.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -40,8 +40,14 @@ var serverCmdArgs = struct {
 func run(cmd *cobra.Command, args []string) error {
 	http.HandleFunc("/", fooHandler)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", serverCmdArgs.port), nil)
-	if err != nil {
+	headerTimeout := time.Minute
+	s := http.Server{
+		Addr:              fmt.Sprintf(":%d", serverCmdArgs.port),
+		Handler:           nil,
+		ReadHeaderTimeout: headerTimeout,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
 		return fmt.Errorf("error serving on port %d: %w", serverCmdArgs.port, err)
 	}
 
